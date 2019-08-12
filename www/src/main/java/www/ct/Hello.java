1b2cf70ccc296116f753aba6ea86b25485144b05
zhengbei <zbhoney@163.com>
2019-08-12 14:14:35 +0800

    cimmit

---
+++ www/src/main/java/www/ct/Hello.java
@@ -0,0 +1,12 @@
+package www.ct;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/aa")
+    public String gete(){
+        return "hello";
+    }
+}
